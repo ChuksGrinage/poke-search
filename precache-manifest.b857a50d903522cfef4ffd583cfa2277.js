5b2e764260afa6821b0760480614a3931a809469
Chuks Grinage <chuksgrinage@gmail.com>
2019-10-31 09:36:04 -0500

    Updates

---
+++ precache-manifest.b857a50d903522cfef4ffd583cfa2277.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebec7c196282d02af2c235e9752977ef",
+    "url": "/poke-search/index.html"
+  },
+  {
+    "revision": "95eba729ea4c003da7a4",
+    "url": "/poke-search/static/css/main.0e6992c4.chunk.css"
+  },
+  {
+    "revision": "923f3f17f2ea591a4e99",
+    "url": "/poke-search/static/js/2.10ef60b1.chunk.js"
+  },
+  {
+    "revision": "95eba729ea4c003da7a4",
+    "url": "/poke-search/static/js/main.946d202b.chunk.js"
+  },
+  {
+    "revision": "45396e4f799949a89c79",
+    "url": "/poke-search/static/js/runtime-main.f9a0c84a.js"
+  },
+  {
+    "revision": "4141190d56795019201251dfa933e997",
+    "url": "/poke-search/static/media/loop2.4141190d.svg"
+  },
+  {
+    "revision": "82d30921a154eb39faa7e534380110c3",
+    "url": "/poke-search/static/media/search.82d30921.svg"
+  }
+]);
